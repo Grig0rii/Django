2237a7eacc146c07af572d2c52946457c26815ce
Grig0rii <shunin.grigorii@mail.ru>
2025-04-04 19:43:54 +0300

    Первоначальная загрузка Django Todo List

---
+++ tasks/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from .models import Task
+from .forms import TaskForm
+
+def task_list(request):
+    tasks = Task.objects.all().order_by('-created_at')
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+def task_detail(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    return render(request, 'tasks/task_detail.html', {'task': task})
+
+def task_create(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Задача успешно создана!')
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'tasks/task_form.html', {'form': form})
+
+def task_update(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Задача успешно обновлена!')
+            return redirect('task_list')
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'tasks/task_form.html', {'form': form})
+
+def task_delete(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        task.delete()
+        messages.success(request, 'Задача успешно удалена!')
+        return redirect('task_list')
+    return render(request, 'tasks/task_confirm_delete.html', {'task': task})
