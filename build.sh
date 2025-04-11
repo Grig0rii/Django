03a48fed553318a856eeeeca2ace72e427b2498b
Grig0rii <shunin.grigorii@mail.ru>
2025-04-11 19:44:50 +0300

    Добавлен скрипт создания суперпользователя и обновлен build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,5 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py migrate 
-
-# Проверяем наличие суперпользователя и создаем его, если его нет
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell 
+python manage.py migrate
+python create_superuser.py 
