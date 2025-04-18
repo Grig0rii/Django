9791eebe8f7001446328cc224ebdfb913d335810
Grig0rii <shunin.grigorii@mail.ru>
2025-04-18 18:05:42 +0300

    Add build.sh for Render deployment

--- build.sh
+++ build.sh
@@ -2,8 +2,16 @@
 # exit on error
 set -o errexit
 
+# Обновление pip
+python -m pip install --upgrade pip
+
+# Установка зависимостей
 pip install -r requirements.txt
 
+# Сбор статических файлов
 python manage.py collectstatic --no-input
+
+# Применение миграций
 python manage.py migrate
+
 python create_superuser.py 
